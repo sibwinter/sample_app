53c192c0eccbdba55aff9f3253d74b7347c2214a
sibwinter <sibwinter42@gmail.com>
2014-09-22 15:16:58 +0800

    сделали базовую модель Юзера. включая безопасный пароль

--- Gemfile
+++ Gemfile
@@ -4,16 +4,20 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.6'
 gem 'bootstrap-sass', '2.3.2.0'
+gem 'bcrypt-ruby'
 
 # Use sqlite3 as the database for Active Record
 group :development, :test do
   gem 'sqlite3', '1.3.8'
-  gem 'rspec-rails', '2.13.1'
+  gem 'rspec-rails'
+  gem 'rspec'
 end
+
+gem 'factory_girl_rails', require: false 
 
 group :test do
   gem 'selenium-webdriver', '2.35.1'
-  gem 'capybara', '2.1.0'
+  gem 'capybara'
 end
 
 gem 'sass-rails', '4.0.1'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,8 +10,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
-
+ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # ## Mock Framework
   #
@@ -40,4 +39,5 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
   config.include Capybara::DSL
+  require 'factory_girl_rails'
 end
